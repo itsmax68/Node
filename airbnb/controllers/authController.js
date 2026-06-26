8d9044ccf51ab7744c81492e3d1f6fc0ecb5a9f3
Manav <manavkalathiya828@gmail.com>
2026-06-26 18:18:38 +0530

    improve ui much better and user validations of express

--- airbnb/controllers/authController.js
+++ airbnb/controllers/authController.js
@@ -1,3 +1,5 @@
+const { body, validationResult } = require('express-validator');
+
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login to airbnb",
@@ -21,3 +23,39 @@ exports.postLogout = (req, res, next) => {
     res.redirect("/login");
   });
 };
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Register to airbnb",
+    currentPage: "signup",
+    isLoggedIn:false
+  });
+};
+
+exports.postSignup = [
+  body('firstName').trim().not().isEmpty().withMessage("First name is required"),
+  body('lastName').trim().not().isEmpty().withMessage("Last name is required"),
+  body('email').trim().isEmail().withMessage("Invalid email"),
+  body('password').trim().isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+  body('confirmPassword').trim().custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
+  body('userType').trim().not().isEmpty().withMessage("User type is required"),
+  body('terms').trim().not().isEmpty().withMessage("Terms are required"),
+  (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render('auth/signup', {
+      pageTitle: "Register to airbnb",
+      currentPage: "signup",
+      isLoggedIn:false,
+      errors: errors.array(),
+      oldInput: req.body
+    });
+  }
+  console.log(req.body);
+  res.redirect("/login");
+}];
